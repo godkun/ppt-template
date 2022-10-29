5259c9825769844ced2ff14347f4a57eef795a15
godkun <godkun666@126.com>
2022-10-29 12:05:40 +0800

    fix: fix some code

--- custom-nav-controls.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <button class="icon-btn" title="Next">
-    <carbon:arrow-right />
-  </button>
-</template>
--- global-bottom.vue
+++ global-bottom.vue
@@ -1,16 +1,7 @@
 <template>
-  <footer
-    class="bottom absolute bottom-0 left-0 p-2"
-  >
-  </footer>
+  <div></div>
 </template>
 
 <script setup lang="ts"></script>
 
-<style>
-.bottom {
-  color: rgb(55, 95, 255);
-  z-index: 1000;
-  background-color: white;
-}
-</style>
+<style></style>
